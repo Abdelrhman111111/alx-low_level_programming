ccd176e500cb2cd7b39f6a329a7a546edfb3d20d
abdelrhman <abdelrhmanmohmed.m14@gmail.com>
2023-02-17 10:54:09 -0800

    d

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+/*
+ * 2-print_alphabet.c
+ * by Abdelrhman
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
