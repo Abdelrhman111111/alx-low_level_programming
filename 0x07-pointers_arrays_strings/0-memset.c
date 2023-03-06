b01b5a41c6dac3cfc2f1dd28134920f37ef61a6e
abdelrhman <abdelrhmanmohmed.m14@gmail.com>
2023-03-06 04:48:11 -0800

    done

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - Entary point.
+ * @s: destination.
+ * @b: constant bytes.
+ * @n: byte.
+ * Return: retuen 0.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+		s[x] = b;
+	return (s);
+}
