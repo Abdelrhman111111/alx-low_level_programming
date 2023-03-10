aa3dc78230e8b8e9eb082c053d595a6225bad196
abdelrhman <abdelrhmanmohmed.m14@gmail.com>
2023-03-10 10:57:53 -0800

    d

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,34 +4,35 @@
  * @argv : value of argumenst.
  * Return: return 0.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-        int n, j, r;
-        int centss[] = {25, 10, 5, 2, 1};
+	int n, j, r;
+	int c[] = {25, 10, 5, 2, 1};
 
-        if (argc != 2)
-        {
-                printf("Error\n");
-                return (1);
-        }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-        n = atoi(argv[1]);
-        r = 0;
+	n = atoi(argv[1]);
+	r = 0;
 
-        if (n < 0)
-        {
-                printf("0\n");
-                return (0);
-        }
+	if (n < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-        for (j = 0; j < 5 && n >= 0; j++)
-        {
-                while (n >= cents[j])
-                {
-                        r++;
-                        n -= cents[j];
-                }
-        }
+	for (j = 0; j < 5 && n >= 0; j++)
+	{
+		while (n >= c[j])
+		{
+			r++;
+			n -= c[j];
+		}
+	}
 
-        printf("%d\n", r);
-        return (0);
+	printf("%d\n", r);
+	return (0);
+}
