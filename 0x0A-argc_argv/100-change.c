c98500e1e6e0f5e34397d600e86c8d84562632b6
abdelrhman <abdelrhmanmohmed.m14@gmail.com>
2023-03-10 11:09:30 -0800

    d

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
 /**
  * main - shows the command line arguments.
  * @argc : number of arguments
@@ -6,8 +10,8 @@
  */
 int main(int argc, char **argv)
 {
-	int num, j, result;
-	int coins[] = {25, 10, 5, 2, 1};
+	int n, i, r;
+	int c[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
@@ -15,24 +19,24 @@ int main(int argc, char **argv)
 		return (1);
 	}
 
-	num = atoi(argv[1]);
-	result = 0;
+	n = atoi(argv[1]);
+	r = 0;
 
-	if (num < 0)
+	if (n < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	for (j = 0; j < 5 && num >= 0; j++)
+	for (i = 0; i < 5 && n >= 0; i++)
 	{
-		while (num >= coins[j])
+		while (n >= c[j])
 		{
-			result++;
-			num -= coins[j];
+			r++;
+			n -= c[j];
 		}
 	}
 
-	printf("%d\n", result);
+	printf("%d\n", r);
 	return (0);
 }
