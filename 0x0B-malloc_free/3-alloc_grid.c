fe4dc7c63c75ff3ab2229de9af3c1a4b51ea05d0
abdelrhman <abdelrhmanmohmed.m14@gmail.com>
2023-03-13 07:37:41 -0700

    d

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * alloc_grid - allocate grid
+  * @width: input width
+  * @height: input height
+  *
+  * Return: pointer
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int x, y, z, l;
+	int **p;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	p = malloc(sizeof(int *) * height);
+
+	if (p == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
+
+	for (x = 0; x < height; x++)
+	{
+		p[x] = malloc(sizeof(int) * width);
+
+		if (p[x] == NULL)
+		{
+			for (y = x; y >= 0; y--)
+			{
+				free(a[y]);
+			}
+
+			free(p);
+			return (NULL);
+		}
+	}
+
+	for (z = 0; z < height; z++)
+	{
+		for (l = 0; l < width; l++)
+		{
+			p[z][l] = 0;
+		}
+	}
+
+	return (p);
+}
