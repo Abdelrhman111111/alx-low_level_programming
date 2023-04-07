61cce8d154aa2683468bb0c70b6f14329c240133
abdelrhman <abdelrhmanmohmed.m14@gmail.com>
2023-04-07 06:43:14 -0700

    done

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 
 /**
- * clear_bit - ....................
- * @n:.......................
- * @intdex: ...............
- * Return:.....................
+ * clear_bit - ...................
+ * @n: ......................
+ * @dex:....................
+ * Return:.........................
  */
 
-int clear_bit(unsigned long int *n, unsigned intdex)
-{	
-
-	if (intdex > sizeof(n) * 8)
+int clear_bit(unsigned long int *n, unsigned int dex)
+{
+	if (dex > 64)
+	{
 		return (-1);
-	*n &= ~(1 << intdex);
-	return (1);
+	}
+	else
+	{
+		*n = *n & ~(1 << dex);
+		return (1);
+	}
 }
