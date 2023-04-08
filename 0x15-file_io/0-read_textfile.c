10e6eb60050e72dcf0dd2047c772b7b5f695bb0a
abdelrhman <abdelrhmanmohmed.m14@gmail.com>
2023-04-08 08:15:12 -0700

    done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * read_textfile -..................
+ * @filename: ...................................
+ * @letters: .....................
+ * Return: ..............................
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int d, x, m;
+	char *b;
+
+	if (!filename)
+		return (0);
+	d = open(filename, O_RDONLY);
+	if (d < 0)
+		return (0);
+	b = malloc(sizeof(char) * letters);
+	if (!b)
+		return (0);
+	i = read(d, b, letters);
+	if (x < 0)
+	{
+		free(b);
+		return (0);
+	}
+	b[x] = '\0';
+	close(d);
+	m = write(STDOUT_FILENO, b, x);
+	if (m < 0)
+	{
+		free(b);
+		return (0);
+	}
+	free(b);
+	return (m);
+}
